400d6247979de025140f87977a01424af9674867
My Name <altynbek.mannapov@mail.ru>
2021-01-30 15:07:38 +0600

    Create Product and Rating Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Product from "./Components/Product";
+import "./data.js";
+import data from "./data.js";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      <div className="grid-template">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              MSH
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <div>
+            <div className="row center">
+              {data.products.map((product) => (
+                <Product key={product._id} product={product}></Product>
+              ))}
+              ;
+            </div>
+          </div>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
     </div>
   );
 }
